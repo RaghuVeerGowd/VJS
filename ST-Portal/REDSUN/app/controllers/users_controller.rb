b566dc75d5bafa5ad48dc02f2a6507e0bec9c03d
RaghuVeerGowd <raghuraas@gmail.com>
2016-07-12 14:44:18 +0530

    fixing login page

--- ST-Portal/REDSUN/app/controllers/users_controller.rb
+++ ST-Portal/REDSUN/app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
 	def show
     @user = User.find(params[:id])		
 	end
@@ -6,7 +12,57 @@ class UsersController < ApplicationController
 	def index
     @users = User.all 		
 	end
-	def edit
-		@user = User.find(params[:id])	
+
+	def edit	
 	end
+  
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1
+  # DELETE /posts/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:email,:password,:first_name,:phone_number,:register_number,:joining_date,:course,:date_of_birth,:gender,:blood_group,:birth_place,:nationality,:mother_tongue,:category,:religion,:present_address,:permanent_address,:city,:pincode,:country,:state,:father_name,:father_job,:mother_name,:mother_job,:previous_institute_name,:previous_institute_address,:previous_institute_grade)
+  end
 end
+
